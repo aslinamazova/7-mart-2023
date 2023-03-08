862418167446663d33affc2e944642d95dfe08e7
aslinamazova <76001yw@code.edu.az>
2023-03-08 19:45:28 +0400

    first commit

---
+++ Task 3.cs
@@ -0,0 +1,12 @@
+﻿int num = 12;
+for (int i = 2; num <= 2;)
+{
+    if (num % 2 == 0)
+    {
+        Console.WriteLine("2-nin quvvetidir");
+    }
+    else
+    {
+        Console.WriteLine("2-nin quvveti deyil");
+    }
+}
---
+++ Task 4.cs
@@ -0,0 +1,9 @@
+﻿int num = 122;
+for (int i = 10; true;)
+{
+    if (num/i>10) 
+    {
+       
+    }
+    if ( num > sum;){
+}
